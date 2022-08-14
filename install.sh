bf8fd9c00bf4fe7c588ab7cbbe16c9b58338f887
noncog <108415765+noncog@users.noreply.github.com>
2022-08-14 01:01:54 -0400

    Added check to ensure script runs after reboot

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root." && exit 1
+if [ ! -f $HOME/resume-after-reboot ]; then
+    if [[ $EUID -ne 0 ]]; then
+        echo "This script must be run as root."
+	  exit 1
+    fi
 fi
 
 debian_sources=(
